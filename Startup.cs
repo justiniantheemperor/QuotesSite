c68d9202abfeb0b0428151686975099d5fb85cdc
justiniantheemperor <justinrellis52@gmail.com>
2022-04-12 23:26:38 -0600

    crud app working, not auto incrementing id

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using QuotesSite.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,14 @@ namespace QuotesSite
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<QuotesContext>(options =>
+            {
+                options.UseSqlite(Configuration["ConnectionStrings:QuotesDBConnection"]);
+            });
+
+            services.AddScoped<IQuotesRepository, EFQuotesRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,7 +60,7 @@ namespace QuotesSite
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Quotes}/{action=Index}/{id?}");
             });
         }
     }

a7dab5746ea4e5d9c71e308ff529390aa337f21c
justiniantheemperor <justinrellis52@gmail.com>
2022-04-12 22:31:08 -0600

    Add project files.

---
+++ Models/Quotes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuotesSite.Models
+{
+    public class Quotes
+    {
+        [Key]
+        [Required]
+        public int QuoteID { get; set; }
+        [Required]
+        public string Quote { get; set; }
+        [Required]
+        public string Author { get; set; }
+        //public DateTime Date { get; set; }
+        public string Subject { get; set; }
+        public string citation { get; set; }
+    }
+}
